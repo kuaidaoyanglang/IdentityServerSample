c69b93a0845d01dc5269fae98049195367d792dc
Wei Yang <yang.wei1@hotmail.com>
2020-01-27 21:29:27 +0800

    实现代码thirdpartdemo

--- ThirdPartDemo/Program.cs
+++ ThirdPartDemo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
 
 namespace ThirdPartDemo
 {
@@ -6,7 +9,51 @@ namespace ThirdPartDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 从元数据中发现终结点,查找IdentityServer(5000端口)
+            var client = new HttpClient();
+            var disco = client.GetDiscoveryDocumentAsync("http://localhost:5000").Result;
+            
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            Console.WriteLine(disco.HttpResponse.Content.ReadAsStringAsync().Result);
+            //向IdentityServer请求令牌
+            var tClient = new HttpClient();
+            tClient.BaseAddress =new Uri("http://localhost:5000/connect/token");
+            var tokenClient = new TokenClient(tClient, new TokenClientOptions()
+            {
+                ClientId = "client",
+                ClientSecret = "secret"
+            });
+            //var tokenResponse = tokenClient.RequestClientCredentialsAsync("api");
+            var tokenResponse = tokenClient.RequestClientCredentialsTokenAsync("api").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            
+            Console.WriteLine(tokenResponse.Json);
+
+            //访问Api
+            //var client = new HttpClient();
+            //把令牌添加进请求
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = client.GetAsync("http://localhost:5002/WeatherForecast").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
         }
     }
 }
