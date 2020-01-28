83954a939dc991922bbd7900f88eb5b95340e85d
Wei Yang <yang.wei1@hotmail.com>
2020-01-28 17:07:41 +0800

    dddddddddddddddd

---
+++ IdentityServerSample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using IdentityServerSample.Models;
+
+namespace IdentityServerSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TestUserStore _users;
+
+
+        public HomeController(ILogger<HomeController> logger,TestUserStore users)
+        {
+            _logger = logger;
+            _users = users;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
